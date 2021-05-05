eff904515318567e49b3c146b7806a2c32f0468a
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-05 11:16:39 +0530

    added new resourses and updated constants

--- shared/constants.js
+++ shared/constants.js
@@ -5,19 +5,21 @@ export default {
   __ALTERNATE_BACKGROUND_COLOR__: '#0D7538',
 
   __PRIMARY_TEXT_COLOR__: '#0D7538',
+  __DEFAULT_TEXT_COLOR__: '#333333',
   __ALTERNATE_TEXT_COLOR__: '#fafafa',
 
   __PRIMARY_ERROR_TEXT_COLOR__: '#ff0000',
   __ALTERNATE_ERROR_TEXT_COLOR__: '#ffef00',
 
   __HINT_TEXT_COLOR__: '#ffffff',
-  __BORDER_COLOR__: '#ffffff',
+  __BORDER_COLOR__: '#dadada',
 
   __DEFAULT_HEADING_FONT__: 'VarelaRound-Regular',
   __DEFAULT_ELEMENTS_FONT__: 'WorkSans-Regular',
 
-  __SMALL_FONT_SIZE__: 12,
-  __DEFAULT_FONT_SIZE__: 16,
+  __EXTRA_SMALL_FONT_SIZE__: 12,
+  __SMALL_FONT_SIZE__: 14,
+  __DEFAULT_FONT_SIZE__: 18,
   __LARGE_FONT_SIZE__: 24,
   __EXTRA_LARGE_FONT_SIZE__: 36,
   __SUPER_LARGE_FONT_SIZE__: 81,
@@ -36,7 +38,10 @@ export default {
   __LARGE_ICON_SIZE__: 48,
 
   __SMALL_ELEM_SIZE__: 40,
-  __LARGE_ELEM_SIZE__: 80,
+  __MEDIUM_ELEM_SIZE__: 80,
+  __LARGE_ELEM_SIZE__: 120,
+  __EXTRA_LARGE_ELEM_SIZE__: 160,
+  __SUPER_LARGE_ELEM_SIZE__: 240,
 
   __FONT_WEIGHT_NORMAL__: Platform.OS === 'ios' ? 400 : 'normal',
   __FONT_WEIGHT_BOLD__: Platform.OS === 'ios' ? 700 : 'bold',
--- shared/resources.js
+++ shared/resources.js
@@ -1,3 +1,11 @@
 export default {
+  //branding
   large_logo: require('./../assets/images/logo.png'),
+  //tabs
+  browse: require('./../assets/images/browse.png'),
+  favorite: require('./../assets/images/favorite.png'),
+  profile: require('./../assets/images/profile.png'),
+  search: require('./../assets/images/search.png'),
+  //misc
+  fork_knife: require('./../assets/images/fork-knife.png'),
 };
