4114c0f5659a07d2e4d2c205cbf1d883ad1f13fe
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-06 11:18:36 +0530

    updated URLs for runtime constants

--- shared/gateway.js
+++ shared/gateway.js
@@ -3,5 +3,5 @@ const __BASE_URL__ = 'https://ns-recipe-api.herokuapp.com/';
 export default {
   __LOGIN_AUTH_URL__: __BASE_URL__.concat('auth'),
   __REGISTER_USER_URL__: __BASE_URL__.concat('user'),
-  __RANDOM_ITEMS_URL__: __BASE_URL__.concat('filter/random?limit=6'),
+  __RANDOM_ITEMS_URL__: __BASE_URL__.concat('filter/random?limit={LIMIT}'),
 };
