5213bf81e111fd8be0f4cc6a000e25c021ae4aa2
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-08 13:56:12 +0530

    added service call for details endpoint

--- screens/detailScreen.js
+++ screens/detailScreen.js
@@ -1,14 +1,39 @@
-import React, {Component} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
+import utils from '../shared/components/utils';
+import Gateway from '../shared/gateway';
 
-export class DetailScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    );
-  }
-}
+const DetailScreen = ({route, navigation}) => {
+  const {id, category} = route.params;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = React.useState('');
+  const [recipeDetail, setRecipeDetail] = React.useState([]);
+
+  React.useEffect(() => {
+    setLoading(true);
+    AsyncStorage.getItem('access_token').then(token => {
+      if (token) {
+        fetch(
+          Gateway.__GET_DETAILS_BY_ID_URL__.replace('{ID}', id),
+          utils.injectGetRequestHeader(token),
+        )
+          .then(r => r.json())
+          .then(result => {
+            console.log('Result', result);
+          });
+      }
+    });
+  }, []);
+
+  return (
+    //
+    <View>
+      <Text>
+        Some Text for id: {id} and {category}
+      </Text>
+    </View>
+  );
+};
 
 export default DetailScreen;
