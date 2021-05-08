d896b768c24af8d6b41a5d7daa9f92f28dab553f
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-08 10:50:48 +0530

    removed obsolete styles block

--- screens/registerScreen.js
+++ screens/registerScreen.js
@@ -8,7 +8,6 @@ import {
   KeyboardAvoidingView,
   SafeAreaView,
   ScrollView,
-  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
@@ -202,52 +201,3 @@ const RegisterScreen = props => {
   );
 };
 export default RegisterScreen;
-
-const styles = StyleSheet.create({
-  SectionStyle: {
-    flexDirection: 'row',
-    height: 40,
-    marginTop: 20,
-    marginLeft: 35,
-    marginRight: 35,
-    margin: 10,
-  },
-  buttonStyle: {
-    backgroundColor: '#69a14f',
-    borderWidth: 0,
-    color: '#FFFFFF',
-    borderColor: '#69a14f',
-    height: 40,
-    alignItems: 'center',
-    borderRadius: 30,
-    marginLeft: 35,
-    marginRight: 35,
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  buttonTextStyle: {
-    color: '#FFFFFF',
-    paddingVertical: 10,
-    fontSize: 16,
-  },
-  inputStyle: {
-    flex: 1,
-    color: 'white',
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderWidth: 1,
-    borderRadius: 30,
-    borderColor: '#dadae8',
-  },
-  errorTextStyle: {
-    color: 'red',
-    textAlign: 'center',
-    fontSize: 14,
-  },
-  successTextStyle: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 18,
-    padding: 30,
-  },
-});
