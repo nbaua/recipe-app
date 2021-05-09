8e0547257effd3524fe030561b33626b43d9549e
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-09 10:02:44 +0530

    layout update and updated styles

--- screens/detailScreen.js
+++ screens/detailScreen.js
@@ -1,14 +1,60 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
-import utils from '../shared/components/utils';
+import {
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Times from '../shared/components/times';
+import Constants from '../shared/constants';
 import Gateway from '../shared/gateway';
+import utils from '../shared/utils';
 
 const DetailScreen = ({route, navigation}) => {
   const {id, category} = route.params;
   const [loading, setLoading] = useState(true);
   const [error, setError] = React.useState('');
   const [recipeDetail, setRecipeDetail] = React.useState([]);
+  const styles = StyleSheet.create({
+    container: {alignContent: 'flex-start', flex: 1},
+    backgroundContainer: {
+      flex: 0.33,
+      flexDirection: 'row',
+      maxWidth: '100%',
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    background: {
+      flex: 1,
+      maxWidth: '100%',
+      paddingTop: Constants.__EXTRA_PADDING__ * 2,
+      height: Constants.__SUPER_LARGE_ELEM_SIZE__,
+    },
+    headerContainer: {
+      height: Constants.__EXTRA_LARGE_ELEM_SIZE__,
+      flexWrap: 'nowrap',
+    },
+    title: {
+      maxWidth: '100%',
+      paddingVertical: Constants.__DEFAULT_PADDING__,
+      paddingHorizontal: Constants.__DEFAULT_PADDING__ * 2,
+      height: Constants.__SMALL_ELEM_SIZE__,
+      minHeight: Constants.__MEDIUM_ELEM_SIZE__ / 1.25,
+      maxHeight: Constants.__MEDIUM_ELEM_SIZE__,
+      backgroundColor: Constants.__ALTERNATE_BACKGROUND_COLOR__,
+      fontFamily: Constants.__DEFAULT_HEADING_FONT__,
+      fontSize: Constants.__LARGE_FONT_SIZE__,
+      color: Constants.__ALTERNATE_TEXT_COLOR__,
+    },
+
+    scrollContainer: {
+      flex: 1,
+      paddingHorizontal: Constants.__EXTRA_PADDING__,
+      paddingVertical: Constants.__EXTRA_PADDING__,
+    },
+  });
 
   React.useEffect(() => {
     setLoading(true);
@@ -20,18 +66,36 @@ const DetailScreen = ({route, navigation}) => {
         )
           .then(r => r.json())
           .then(result => {
-            console.log('Result', result);
+            // console.log(result);
+            setRecipeDetail(result);
+          })
+          .catch(e => {
+            setRecipeDetail('');
+            setError('Exception occurred while fetching recipe: ' + e);
           });
       }
     });
   }, []);
 
   return (
-    //
-    <View>
-      <Text>
-        Some Text for id: {id} and {category}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.backgroundContainer}>
+        <ImageBackground
+          style={styles.background}
+          source={{uri: recipeDetail.pictureUrl}}
+          resizeMode="cover">
+          <Text>.</Text>
+        </ImageBackground>
+      </View>
+      <View style={styles.headerContainer}>
+        <Text numberOfLines={3} style={styles.title}>
+          {recipeDetail.name}
+        </Text>
+        {recipeDetail.times && <Times data={recipeDetail.times} />}
+      </View>
+      <ScrollView style={styles.scrollContainer}>
+        <Text>{recipeDetail.description}</Text>
+      </ScrollView>
     </View>
   );
 };
