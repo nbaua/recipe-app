bf5582c3675310d7abf4d57e0132365b3fad72be
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-11 10:26:53 +0530

    added the new recipe by id endpoint

--- shared/gateway.js
+++ shared/gateway.js
@@ -1,4 +1,4 @@
-const __BASE_URL__ = 'https://ns-recipe-api.herokuapp.com/';
+const __BASE_URL__ = 'http://172.17.238.17:3000/'; //'https://ns-recipe-api.herokuapp.com/';
 
 export default {
   __LOGIN_AUTH_URL__: __BASE_URL__.concat('auth'),
@@ -7,5 +7,5 @@ export default {
   __RANDOM_BY_CATEGORY_URL__: __BASE_URL__.concat(
     'filter/category?limit={LIMIT}&category={CATEGORY}',
   ),
-  __GET_DETAILS_BY_ID_URL__: __BASE_URL__.concat('recipe/{ID}'),
+  __GET_DETAILS_BY_ID_URL__: __BASE_URL__.concat('filter/recipe?id={ID}'),
 };
